c12e6293c8ac1a2db9f07e9b2691ee198eaa76bd
pramod-android <40615535+pramod-android@users.noreply.github.com>
2018-07-23 09:47:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/win/retrofitdemoapplication/ApiClient.java
@@ -0,0 +1,16 @@
+package com.example.win.retrofitdemoapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiClient {
+
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+
+
+}
